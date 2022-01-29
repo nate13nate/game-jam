90b83e74c508d081072205cadcf3f87e6cb07614
Nate <nate13nate@gmail.com>
2022-01-29 16:53:53 -0600

    Changed grounded to bool and put horizontal movement into fixedupdate function

--- Assets/Scenes/playerMove.cs
+++ Assets/Scenes/playerMove.cs
@@ -4,41 +4,33 @@ using UnityEngine;
 
 public class playerMove : MonoBehaviour
 {
-    public float speed;
-    public float jump;
-    
-    private int grounded;
-    private float move;
-    private Rigidbody2D rigidbody2;
-    // Start is called before the first frame update
-    void Start()
-    {
-        //start grounded so player can jump & find rigidbody for transformations
-        grounded = 1;
-        rigidbody2 = GetComponent<Rigidbody2D>();
-    }
+  public float speed;
+  public float jump;
 
-    // Update is called once per frame
-    void Update()
-    {
-        //horizontal movement
-        move = Input.GetAxis("Horizontal");
-        rigidbody2.velocity = new Vector2(move * speed, rigidbody2.velocity.y);
+  private bool grounded = true; // start grounded so player can jump & find rigidbody for transformations
+  private float move;
+  private Rigidbody2D rigidbody2;
 
+  // Start is called before the first frame update
+  void Start()
+  {
+    rigidbody2 = GetComponent<Rigidbody2D>();
+  }
 
-        //find if player is moving vertically, if not moving vertically then allow jump, otherwise dont
-        if (rigidbody2.velocity.y == 0f){
-            grounded = 1;
-        }else{
-            grounded = 0;
-        }
-        if (Input.GetButtonDown("Jump") && grounded == 1)
-        {
-            rigidbody2.AddForce(new Vector2(rigidbody2.velocity.x, jump));
-        }
-    }
+  // Update is called once per frame
+  void Update()
+  {
+    //horizontal movement
+    move = Input.GetAxis("Horizontal");
 
-    
+    //find if player is moving vertically, if not moving vertically then allow jump, otherwise dont
+    grounded = rigidbody2.velocity.y == 0f;
 
-    
+    if (Input.GetButtonDown("Jump") && grounded) rigidbody2.AddForce(new Vector2(0, jump));
+  }
+
+  void FixedUpdate()
+  {
+    rigidbody2.velocity = new Vector2(move * speed, rigidbody2.velocity.y);
+  }
 }
