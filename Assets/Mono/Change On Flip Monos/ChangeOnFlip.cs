31356b2c51a76d663700bb42a9105de590e26e94
JWarfield815 <creepathan@gmail.com>
2022-01-30 14:10:38 -0600

    killing works pogpogpogpogpog

--- Assets/Mono/Change On Flip Monos/ChangeOnFlip.cs
+++ Assets/Mono/Change On Flip Monos/ChangeOnFlip.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class ChangeOnFlip : MonoBehaviour
 {
   public UnityEvent onFlipEvent;
   private SpriteRenderer platform;
   private bool active = false;
+  private bool canKill = false;
 
   private void Start() {
     platform = GetComponent<SpriteRenderer>();
@@ -16,11 +18,17 @@ public class ChangeOnFlip : MonoBehaviour
   public void flip() {
     if (active) {
       onFlipEvent.Invoke();
-      platform.color = new Color(0.58f, 0, 0.83f, 1);
+      canKill = true;
     }
   }
 
   public void setActive(bool thing) {
     active = thing;
+  }
+
+  public void murderPlayer() {
+    if (canKill) {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
   }
 }
